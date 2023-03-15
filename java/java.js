231150c803d4708acb3adc6f6b04fd6dd2fb0b9d
Deja_C <dejacowart@gmail.com>
2023-03-15 07:22:04 -0400

    changes to the javascript implementing functionality

--- java/java.js
+++ java/java.js
@@ -1,11 +1,46 @@
-// When clicking on the start button the button is removed and a question pops up with a list of answers and a timer that counts down
-
-// Targeting the start button
+var myQuestions = [
+    {
+            id:0,
+            question: "Who is identified as the lone land animal in Bikini Bottom?",
+            option:["Spongebob","Patrick","Sandy"],
+            answer:"Sandy"
+    },
+    {
+        id:2,
+        question: "What shirt color does SpongeBob usually wear?",
+        option:["Blue","White","Brown"],
+        answer:"White"
+    },
+    {
+        id:3,
+        question: "Who has a wife named Karen?",
+        option:["Mr.Krabs","Squidward","Plankton"],
+        answer:"Plankton"
+    },
+    {
+        id:4,
+        question: "What color are Spongebob's eyes?",
+        option:["Green","Blue","Brown"],
+        answer:"Blue"
+    },{
+    id:5,
+    question: "In the SpongeBob SquarePants movie, what do SpongeBob and Patrick use to drown their sorrows?",
+    option:["Ice-Cream","Jelly-Fishing","Bubble-blowing"],
+    answer:"Ice-Cream"
+},]
+    
+// Getting the result display section
+var result = document.getElementsByClassName(".result");
+    result[0].innerText = "";
+// // Getting the question and setting the question
+var question = document.getElementById("question");
+    question.innerText = myQuestions[id].question;
 var startButton = document.getElementById("start-button");
 var timerElement = document.querySelector(".timer-count");
 var timerCount = 60;
 var timer;
 
+// Creating timer
 function startTimer() {
     // Sets timer
     timer = setInterval(function() {
@@ -19,7 +54,7 @@ function startTimer() {
     }, 1000);
 };
 
-// When the start button is clicked the button is removed
+// When the start button is clicked the start button is removed and the timer starts.
 startButton.addEventListener("click", function() {
     startButton.remove();
     startTimer();
